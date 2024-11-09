9f23341b73ccda90cb15012e8c40e21caa7cf787
Chromoprotein <bridges.of.sighs@gmail.com>
2024-11-09 22:25:42 +0200

    added a download button for images

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,19 +1,33 @@
 import Markdown from 'react-markdown'
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { FaRegCopy } from "react-icons/fa";
 import { FaRegCheckCircle } from "react-icons/fa";
+import { IoMdDownload } from "react-icons/io";
 
-export default function Message({message, index}) {
+export default function Message({message, messageIndex}) {
 
-    const [copySuccess, setCopySuccess] = useState('');
+    const [copySuccess, setCopySuccess] = useState(false);
+
+    const parentDivRef = useRef(null);
+
+    // Downloading an image
+    const downloadImage = () => {
+      const image = parentDivRef.current.querySelector('img');
+      const link = document.createElement("a");
+      link.href = image;
+      link.target = "_blank";
+      link.download = "generated_image.png";
+      link.click();
+    }
 
     // Copying a message
     const copyToClipboard = (message) => {
       navigator.clipboard.writeText(message)
-        .then(() => setCopySuccess('Copied!'))
-        .catch((err) => setCopySuccess('Failed to copy'));
+        .then(() => setCopySuccess(true))
+        .catch((err) => setCopySuccess(false));
     };
 
+    // Extract the text and (user-uploaded) image variables from the message content
     const mapItems = message.content.map((item) => {
       let text = null;
       let image = null;
@@ -26,13 +40,31 @@ export default function Message({message, index}) {
     });
 
     return (
-      <div key={index} className="message">
+      <div key={messageIndex} className="message" id={messageIndex} ref={parentDivRef}>
+          {/*sender name*/}
           <span className="name">
             {message.role === "user" ? "You: " : "Mysterious traveller: "}
           </span>
 
-          {mapItems[0].text && <Markdown>{mapItems[0].text}</Markdown>}
+          {/*render text and AI images with markdown. Images have a download button*/}
+          {mapItems[0].text && 
+            <Markdown
+              components={{
+                img: ({ node, ...props }) => (
+                  <div className="imageContainer">
+                    <img {...props} alt="AI generated" />
+                    <button className="downloadButton" onClick={downloadImage} >
+                      <IoMdDownload size={30} />
+                    </button>
+                  </div>
+                ),
+              }}
+            >
+              {mapItems[0].text}
+            </Markdown>
+          }
 
+          {/*if the user uploaded an image, display it too*/}
           {mapItems[0].image && 
             <img
               src={mapItems[0].image}
@@ -40,9 +72,9 @@ export default function Message({message, index}) {
             />
           }
 
+          {/*if there is text, display a button for copying the text at the bottom of the message*/}
           {mapItems[0].text && 
-            <CopyButton copyToClipboard={copyToClipboard} copySuccess={copySuccess} text={mapItems[0].text}
-          />}
+            <CopyButton copyToClipboard={copyToClipboard} copySuccess={copySuccess} text={mapItems[0].text}/>}
 
       </div> 
     );
